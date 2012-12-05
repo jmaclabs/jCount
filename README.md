6e24e172165ae11c27deaaf5900da147d0583376
John McLaughlin <jmac@gravity.com>
2012-12-04 21:03:22 -0800

    initial code commit
    adding usage example
    
    includes jquery and jcount.js
    
    updates for attribution and usage

--- README.md
+++ README.md
@@ -2,3 +2,84 @@ jCount
 ======
 
 jquery plugin counter with number formatting
+
+NOTICE:
+------
+
+The jCount plugin was originally created and generously shared by [Matt Huggins](https://github.com/mhuggins/jquery-countTo).  
+
+Matt is awesome.
+
+I have added a function to Matt's plugin that formats the numbers (adds commas).  Some people think that's awesome too.
+
+For the whole story, read this blog post: [http://johnmclaughlin.info/introducing-jcount-a-jquery-countup-plugin-with-output-number-formatting/](http://johnmclaughlin.info/introducing-jcount-a-jquery-countup-plugin-with-output-number-formatting/)
+
+Requirements:
+-------------
+[jCount](https://github.com/jmaclabs/jCount) requires the latest version of [jQuery](http://jquery.com).
+
+Usage:
+------
+The most basic usage of this plugin is to create a DOM element, and apply the `countTo` method to it in your JS.
+
+    <script type="text/javascript"><!--
+        $('.timer').countTo({from: 0, to: 500});
+    //--></script>
+    
+    <span class="timer"></span>
+
+A more detailed example that demonstrates all possible options being used is as follows.
+
+    <script type="text/javascript"><!--
+        $('.timer').countTo({
+            from: 50,
+            to: 2500,
+            speed: 1000,
+            refreshInterval: 50,
+            onComplete: function(value) {
+                console.debug(this);
+            }
+        });
+    //--></script>
+    
+    <span class="timer"></span>
+
+Options:
+--------
+A complete listing of the options that can be passed to the `countTo` method is below.
+
+<table>
+  <tr>
+    <th>Option</th>
+    <th>Description</th>
+  </tr>
+  <tr>
+    <td><i>from</i></td>
+    <td>The number to start counting from.</td>
+  </tr>
+  <tr>
+    <td><i>to</i></td>
+    <td>The number to stop counting at.</td>
+  </tr>
+  <tr>
+    <td><i>speed</i></td>
+    <td>The number of milliseconds it should take to finish counting.</td>
+  </tr>
+  <tr>
+    <td><i>refreshInterval</i></td>
+    <td>The number of milliseconds updating the count.</td>
+  </tr>
+  <tr>
+    <td><i>onComplete</i></td>
+    <td>A callback function the is triggered when counting finishes.  The final value is
+        passed to the function, and it is run in the context of the DOM element</td>
+  </tr>
+</table>
+
+Created By:
+-----------
+[Matt Huggins](http://www.matthuggins.com)
+
+License:
+--------
+jCount and jQuery-countTo are released under the [MIT license](http://www.opensource.org/licenses/MIT).
---
+++ jcount.js
@@ -0,0 +1,83 @@
+/**
+ * Author: Matt Huggins (with updates from John McLaughlin)
+ * Date: 10/10/11
+ * Time: 5:00 PM
+ * Desc: jCount - jquery plugin counter with number formatting
+ *
+ * See the NOTICE file distributed with this work for additional information
+ * regarding the origin of this work.  
+ * 
+ * Otherwise, I license this file to you use under the Apache License, 
+ * Version 2.0 (the License); you may not this file except in compliance with 
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an AS IS BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+(function($) {
+
+    $.fn.countTo = function(options) {
+        // merge the default plugin settings with the custom options
+        options = $.extend({}, $.fn.countTo.defaults, options || {});
+
+        // how many times to update the value, and how much to increment the value on each update
+        var loops = Math.ceil(options.speed / options.refreshInterval),
+            increment = (options.to - options.from) / loops;
+
+        return $(this).each(function() {
+            var _this = this,
+                loopCount = 0,
+                value = options.from,
+                interval = setInterval(updateTimer, options.refreshInterval);
+
+            function updateTimer() {
+                value += increment;
+                loopCount++;
+                $(_this).html(addCommas(value.toFixed(options.decimals)))
+
+                if (typeof(options.onUpdate) == 'function') {
+                    options.onUpdate.call(_this, value);
+                }
+
+                if (loopCount >= loops) {
+                    clearInterval(interval);
+                    value = options.to;
+
+                    if (typeof(options.onComplete) == 'function') {
+                        options.onComplete.call(_this, value);
+                    }
+                }
+            }
+
+            function addCommas(nStr){
+              nStr += '';
+              x = nStr.split('.');
+              x1 = x[0];
+              x2 = x.length > 1 ? '.' + x[1] : '';
+              var rgx = /(\d+)(\d{3})/;
+              while (rgx.test(x1)) {
+                x1 = x1.replace(rgx, '$1' + ',' + '$2');
+              }
+              return x1 + x2;
+            }
+
+        });
+    };
+
+    $.fn.countTo.defaults = {
+        from: 0,  // the number the element should start at
+        to: 100,  // the number the element should end at
+        speed: 1000,  // how long it should take to count between the target numbers
+        refreshInterval: 100,  // how often the element should be updated
+        decimals: 0,  // the number of decimal places to show
+        onUpdate: null,  // callback method for every time the element is updated,
+        onComplete: null  // callback method for when the element finishes updating
+    };
+})(jQuery);
